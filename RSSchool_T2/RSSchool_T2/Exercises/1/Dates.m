61c5a2b4fcb688326a35d5c522be2946bc51e207
Andrey Butcitcyn <btctcn@gmail.com>
2020-03-21 23:55:50 +0300

    add Dates implementation

--- RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
+++ RSSchool_T2/RSSchool_T2/Exercises/1/Dates.m
@@ -1,9 +1,24 @@
+#import <Foundation/Foundation.h>
 #import "Dates.h"
 
 @implementation Dates
 
 - (NSString *)textForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
-    return @"";
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    [formatter setLocale:[NSLocale localeWithLocaleIdentifier:@"ru_RU"]];
+    [formatter setDateFormat:@"d MMMM, EEEE"];
+    
+    NSDate *date = [self dateForDay:day month:month year:year];
+    NSString *result = (date) ? [formatter stringFromDate:date] : @"Такого дня не существует";
+    NSLog(@"%@", result);
+    return result;
+}
+
+- (NSDate *) dateForDay:(NSString *)day month:(NSString *)month year:(NSString *)year {
+    NSDateFormatter *formatter = [NSDateFormatter new];
+    [formatter setDateFormat:@"yyyy-MM-dd"];
+    [formatter setTimeZone:[NSTimeZone timeZoneWithName:@"UTC"]];
+    return [formatter dateFromString:[NSString stringWithFormat:@"%@-%@-%@", year, month, day]];
 }
 
 @end
