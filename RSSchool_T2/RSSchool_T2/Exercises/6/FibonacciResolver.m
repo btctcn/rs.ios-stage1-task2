9de583f5b0cd129f84de2b281fe2ae4b23dd6b3c
Andrey Butcitcyn <btctcn@gmail.com>
2020-03-23 03:52:56 +0300

    add FibonacciResolver implementation

--- RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
+++ RSSchool_T2/RSSchool_T2/Exercises/6/FibonacciResolver.m
@@ -2,6 +2,20 @@
 
 @implementation FibonacciResolver
 - (NSArray*)productFibonacciSequenceFor:(NSNumber*)number {
-    return @[];
+    int num = number.intValue;
+    
+    int mul = 0;
+    int cnt = 2;
+    
+    int first = 0;
+    int second = 1;
+    while((mul = first * second) < num){
+        int next = first + second;
+        first = second;
+        second = next;
+    }
+
+    return @[[NSNumber numberWithInt:first], [NSNumber numberWithInt:second], (first*second == num) ? @1 : @0 ];
+    
 }
 @end
